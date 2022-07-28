e5dc5960ebf48c3b61bf9673c20e4c3ff72c5836
dfsdfsdfgsdfgdsfgsdfg <110134894+dfsdfsdfgsdfgdsfgsdfg@users.noreply.github.com>
2022-07-28 12:53:01 +0200

    Add files via upload

---
+++ kctf_c0d3Q1_p0w3r.java
@@ -0,0 +1,13 @@
+public class kctf_c0d3Q1_p0w3r {
+
+	public void kctf_c0d3q1_4tw() {
+		try {
+			throw null; // quality code...
+		} catch (final Exception kctf_C0d3QL_is_exceptional) {
+			// ignore like a good programemr
+		}
+	}
+
+	public static final String FLAG = "kctf_c0d3Ql_rul3z!";
+
+}
